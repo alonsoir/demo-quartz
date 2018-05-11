4ca223b5957dabf8f2b2b6635324016885ae2edf
Alonso Isidoro Roman <alonsoir@gmail.com>
2018-05-11 17:26:57 +0200

    minor bugs. Now i use internal h2 pojo id for kafka id. It will be the same for ES. Experiment.

--- src/main/java/com/aironman/demoquartz/kafka/MessageProducer.java
+++ src/main/java/com/aironman/demoquartz/kafka/MessageProducer.java
@@ -5,34 +5,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
 public class MessageProducer {
-
+    
     @Autowired
     KafkaTemplate<String, BitcoinEuroKafkaEntity> producer;
     
     @Value(value = "${message.topic.name}")
     private String topicName;
-
+    
     @Value(value = "${partitioned.topic.name}")
     private String partionedTopicName;
-
+    
     @Value(value = "${filtered.topic.name}")
     private String filteredTopicName;
-
-    public void sendMessageToTopic (BitcoinEuroKafkaEntity entity) {
-    	try {
-    		producer.send(topicName,entity);
-    	}catch (Exception e) {
-    		System.out.println("Something went wrong when sending entity to topic: " + e.getMessage());
-    	}catch (Throwable th) {
-    		System.out.println("Something went wrong when sending entity to topic: " + th.getMessage());
-    	}
+    
+    public void sendMessageToTopic(BitcoinEuroKafkaEntity entity) {
+	try {
+	    producer.send(topicName, entity);
+	} catch (Exception e) {
+	    System.out.println("Something went wrong when sending entity to topic: " + e.getMessage());
+	} catch (Throwable th) {
+	    System.out.println("Something went wrong when sending entity to topic: " + th.getMessage());
+	}
     }
     
     public void sendMessageToPartitionedTopicName(BitcoinEuroKafkaEntity entity) {
-    	producer.send(partionedTopicName,entity);
+	producer.send(partionedTopicName, entity);
     }
     
     public void sendMessageToFilteredTopicName(BitcoinEuroKafkaEntity entity) {
-    	producer.send(filteredTopicName,entity);
+	producer.send(filteredTopicName, entity);
     }
 }
--- src/main/java/com/aironman/demoquartz/service/SampleJobService.java
+++ src/main/java/com/aironman/demoquartz/service/SampleJobService.java
@@ -12,86 +12,86 @@ import com.aironman.demoquartz.pojo.BitcoinEuro;
 
 @Service
 public class SampleJobService {
-
-	@Autowired
-	private BitCoinEuroService bitCoinService;
-	
+    
+    @Autowired
+    private BitCoinEuroService bitCoinService;
+    
     private Logger logger = LoggerFactory.getLogger(getClass());
-
     
     public void executeSampleJob() {
-
-        logger.info("The sample job has begun...");
-        try {
-        	RestTemplate restTemplate = new RestTemplate();
-            BitcoinEuro[] bitCoinEuro =  restTemplate.getForObject("https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR", BitcoinEuro[].class);
-            logger.info(bitCoinEuro[0].toString());
-    		BitcoinEuro jsonPojo = bitCoinEuro[0];
-            
-            BitcoinEuroEntity entity = generatePojoFromJson(jsonPojo );
-            BitcoinEuroEntity created = bitCoinService.create(entity);
-            logger.info("created entity...");
-            logger.info(created.toString());
-            
-            BitcoinEuroKafkaEntity kafkaEntity = generateKafkaPojoFromJson(jsonPojo);
-            bitCoinService.sendMessageToTopic(kafkaEntity );
-            logger.info("entity sent to topic...");
-
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            logger.error("Error while executing sample job", e);
-        } finally {
-            logger.info("Sample job has finished...");
-        }
+	
+	logger.info("The sample job has begun...");
+	try {
+	    RestTemplate restTemplate = new RestTemplate();
+	    BitcoinEuro[] bitCoinEuro = restTemplate.getForObject("https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR",
+	            BitcoinEuro[].class);
+	    logger.info(bitCoinEuro[0].toString());
+	    BitcoinEuro jsonPojo = bitCoinEuro[0];
+	    
+	    BitcoinEuroEntity entity = generatePojoFromJson(jsonPojo);
+	    BitcoinEuroEntity created = bitCoinService.create(entity);
+	    logger.info("created entity...");
+	    logger.info(created.toString());
+	    // I want this id for kafka topic, it should be the id for ES too...
+	    jsonPojo.setId(String.valueOf(created.getIdBCEntity()));
+	    BitcoinEuroKafkaEntity kafkaEntity = generateKafkaPojoFromJson(jsonPojo);
+	    bitCoinService.sendMessageToTopic(kafkaEntity);
+	    logger.info("entity sent to topic...");
+	    
+	    Thread.sleep(5000);
+	} catch (InterruptedException e) {
+	    logger.error("Error while executing sample job", e);
+	} finally {
+	    logger.info("Sample job has finished...");
+	}
     }
-
-
-	private BitcoinEuroEntity generatePojoFromJson(BitcoinEuro jsonPojo ) {
-		// It should be used something like dozer to create this pojo from json pojo...
-		BitcoinEuroEntity entity = new BitcoinEuroEntity();
-		entity.set_24hVolumeEur(jsonPojo.get24hVolumeEur());
-		entity.set_24hVolumeUsd(jsonPojo.get24hVolumeUsd());
-		entity.setAvailableSupply(jsonPojo.getAvailableSupply());
-		entity.setId(jsonPojo.getId());
-		entity.setLastUpdated(jsonPojo.getLastUpdated());
-		entity.setMarketCapEur(jsonPojo.getMarketCapEur());
-		entity.setMarketCapUsd(jsonPojo.getMarketCapUsd());
-		entity.setMaxSupply(jsonPojo.getMaxSupply());
-		entity.setName(jsonPojo.getName());
-		entity.setPriceBtc(jsonPojo.getPriceBtc());
-		entity.setPercentChange1h(jsonPojo.getPercentChange1h());
-		entity.setPercentChange24h(jsonPojo.getPercentChange24h());
-		entity.setPercentChange7d(jsonPojo.getPercentChange7d());
-		entity.setPriceEur(jsonPojo.getPriceEur());
-		entity.setPriceUsd(jsonPojo.getPriceUsd());
-		entity.setRank(jsonPojo.getRank());
-		entity.setSymbol(jsonPojo.getSymbol());
-		entity.setTotalSupply(jsonPojo.getTotalSupply());
-		return entity;
-	}
-	
-	private BitcoinEuroKafkaEntity generateKafkaPojoFromJson(BitcoinEuro jsonPojo ) {
-		// It should be used something like dozer to create this pojo from json pojo...
-		BitcoinEuroKafkaEntity entity = new BitcoinEuroKafkaEntity();
-		entity.set_24hVolumeEur(jsonPojo.get24hVolumeEur());
-		entity.set_24hVolumeUsd(jsonPojo.get24hVolumeUsd());
-		entity.setAvailableSupply(jsonPojo.getAvailableSupply());
-		entity.setId(jsonPojo.getId());
-		entity.setLastUpdated(jsonPojo.getLastUpdated());
-		entity.setMarketCapEur(jsonPojo.getMarketCapEur());
-		entity.setMarketCapUsd(jsonPojo.getMarketCapUsd());
-		entity.setMaxSupply(jsonPojo.getMaxSupply());
-		entity.setName(jsonPojo.getName());
-		entity.setPriceBtc(jsonPojo.getPriceBtc());
-		entity.setPercentChange1h(jsonPojo.getPercentChange1h());
-		entity.setPercentChange24h(jsonPojo.getPercentChange24h());
-		entity.setPercentChange7d(jsonPojo.getPercentChange7d());
-		entity.setPriceEur(jsonPojo.getPriceEur());
-		entity.setPriceUsd(jsonPojo.getPriceUsd());
-		entity.setRank(jsonPojo.getRank());
-		entity.setSymbol(jsonPojo.getSymbol());
-		entity.setTotalSupply(jsonPojo.getTotalSupply());
-		return entity;
-	}
-	
+    
+    private BitcoinEuroEntity generatePojoFromJson(BitcoinEuro jsonPojo) {
+	// It should be used something like dozer to create this pojo from json pojo...
+	BitcoinEuroEntity entity = new BitcoinEuroEntity();
+	entity.set_24hVolumeEur(jsonPojo.get24hVolumeEur());
+	entity.set_24hVolumeUsd(jsonPojo.get24hVolumeUsd());
+	entity.setAvailableSupply(jsonPojo.getAvailableSupply());
+	entity.setId(jsonPojo.getId());
+	entity.setLastUpdated(jsonPojo.getLastUpdated());
+	entity.setMarketCapEur(jsonPojo.getMarketCapEur());
+	entity.setMarketCapUsd(jsonPojo.getMarketCapUsd());
+	entity.setMaxSupply(jsonPojo.getMaxSupply());
+	entity.setName(jsonPojo.getName());
+	entity.setPriceBtc(jsonPojo.getPriceBtc());
+	entity.setPercentChange1h(jsonPojo.getPercentChange1h());
+	entity.setPercentChange24h(jsonPojo.getPercentChange24h());
+	entity.setPercentChange7d(jsonPojo.getPercentChange7d());
+	entity.setPriceEur(jsonPojo.getPriceEur());
+	entity.setPriceUsd(jsonPojo.getPriceUsd());
+	entity.setRank(jsonPojo.getRank());
+	entity.setSymbol(jsonPojo.getSymbol());
+	entity.setTotalSupply(jsonPojo.getTotalSupply());
+	return entity;
+    }
+    
+    private BitcoinEuroKafkaEntity generateKafkaPojoFromJson(BitcoinEuro jsonPojo) {
+	// It should be used something like dozer to create this pojo from json pojo...
+	BitcoinEuroKafkaEntity entity = new BitcoinEuroKafkaEntity();
+	entity.set_24hVolumeEur(jsonPojo.get24hVolumeEur());
+	entity.set_24hVolumeUsd(jsonPojo.get24hVolumeUsd());
+	entity.setAvailableSupply(jsonPojo.getAvailableSupply());
+	entity.setId(jsonPojo.getId());
+	entity.setLastUpdated(jsonPojo.getLastUpdated());
+	entity.setMarketCapEur(jsonPojo.getMarketCapEur());
+	entity.setMarketCapUsd(jsonPojo.getMarketCapUsd());
+	entity.setMaxSupply(jsonPojo.getMaxSupply());
+	entity.setName(jsonPojo.getName());
+	entity.setPriceBtc(jsonPojo.getPriceBtc());
+	entity.setPercentChange1h(jsonPojo.getPercentChange1h());
+	entity.setPercentChange24h(jsonPojo.getPercentChange24h());
+	entity.setPercentChange7d(jsonPojo.getPercentChange7d());
+	entity.setPriceEur(jsonPojo.getPriceEur());
+	entity.setPriceUsd(jsonPojo.getPriceUsd());
+	entity.setRank(jsonPojo.getRank());
+	entity.setSymbol(jsonPojo.getSymbol());
+	entity.setTotalSupply(jsonPojo.getTotalSupply());
+	return entity;
+    }
+    
 }
