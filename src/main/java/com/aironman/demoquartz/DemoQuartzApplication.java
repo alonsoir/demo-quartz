33742b00d74ffd0c133f2bc670de78f6a215c30d
Alonso Isidoro Roman <alonsoir@gmail.com>
2018-05-16 20:01:44 +0200

    adding eureka client capability

--- src/main/java/com/aironman/demoquartz/DemoQuartzApplication.java
+++ src/main/java/com/aironman/demoquartz/DemoQuartzApplication.java
@@ -1,12 +1,35 @@
 package com.aironman.demoquartz;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class DemoQuartzApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoQuartzApplication.class, args);
-	}
+    
+    Logger logger = LoggerFactory.getLogger(getClass());
+    
+    public static void main(String[] args) {
+	SpringApplication.run(DemoQuartzApplication.class, args);
+    }
+    
+    @PostConstruct
+    public void doingSomethingCoolWithKafka() {
+	logger.info("INIT doingSomethingCoolWithKafka...");
+	logger.info("END doingSomethingCoolWithKafka...");
+    }
+    
+    @RequestMapping("/greeting")
+    public String greeting() {
+	return "Hello from EurekaClient! This is DemoQuartzApplication...";
+    }
+    
 }
